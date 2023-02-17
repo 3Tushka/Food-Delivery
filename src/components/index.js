6cf2974dbc5444aa0761394c55579ba5fcb3f53e
Andriy Panasiuk <panasiukmail@gmail.com>
2023-02-17 15:11:27 +0200

    project init

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Header } from "./Header/Header.jsx";
+export { default as MainContainer } from "./MainContainer/MainContainer.jsx";
+export { default as CreateContainer } from "./CreateContainer/CreateContainer.jsx";
---
+++ src/constants/images.js
@@ -0,0 +1,121 @@
+import c1 from '../assets/images/c1.png';
+import c2 from '../assets/images/c2.png';
+import c3 from '../assets/images/c3.png';
+import c4 from '../assets/images/c4.png';
+import c6 from '../assets/images/c6.png';
+import c7 from '../assets/images/c7.png';
+
+import cu1 from '../assets/images/cu1.png';
+import cu2 from '../assets/images/cu2.png';
+import cu3 from '../assets/images/cu3.png';
+import cu4 from '../assets/images/cu4.png';
+import cu5 from '../assets/images/cu5.png';
+import cu6 from '../assets/images/cu6.png';
+
+import d1 from '../assets/images/d1.png';
+import d2 from '../assets/images/d2.png';
+import d3 from '../assets/images/d3.png';
+import d4 from '../assets/images/d4.png';
+import d5 from '../assets/images/d5.png';
+import d6 from '../assets/images/d6.png';
+import d7 from '../assets/images/d7.png';
+import d8 from '../assets/images/d8.png';
+
+import f1 from '../assets/images/f1.png';
+import f2 from '../assets/images/f2.png';
+import f3 from '../assets/images/f3.png';
+import f4 from '../assets/images/f4.png';
+import f5 from '../assets/images/f5.png';
+import f6 from '../assets/images/f6.png';
+import f7 from '../assets/images/f7.png';
+import f8 from '../assets/images/f8.png';
+import f9 from '../assets/images/f9.png';
+import f10 from '../assets/images/f10.png';
+
+import fi1 from '../assets/images/fi1.png';
+import fi2 from '../assets/images/fi2.png';
+import fi3 from '../assets/images/fi3.png';
+import fi4 from '../assets/images/fi4.png';
+import fi5 from '../assets/images/fi5.png';
+
+import i1 from '../assets/images/i1.png';
+import i2 from '../assets/images/i2.png';
+import i3 from '../assets/images/i3.png';
+import i4 from '../assets/images/i4.png';
+import i5 from '../assets/images/i5.png';
+import i6 from '../assets/images/i6.png';
+import i7 from '../assets/images/i7.png';
+
+import r1 from '../assets/images/r1.png';
+import r2 from '../assets/images/r2.png';
+import r3 from '../assets/images/r3.png';
+import r4 from '../assets/images/r4.png';
+import r5 from '../assets/images/r5.png';
+
+import NotFound from '../assets/images/NotFound.svg';
+import emptyCart from '../assets/images/emptyCart.svg';
+
+import logo from '../assets/images/logo.png';
+import heroBg from '../assets/images/heroBg.png';
+import delivery from '../assets/images/delivery.png';
+import avatar from '../assets/images/avatar.png';
+import cheff from '../assets/images/cheff.png';
+import chef1 from '../assets/images/chef1.png';
+
+export default {
+    c1,
+    c2,
+    c3,
+    c4,
+    c6,
+    c7,
+    d1,
+    d2,
+    d3,
+    d4,
+    d5,
+    d6,
+    d7,
+    d8,
+    cu1,
+    cu2,
+    cu3,
+    cu4,
+    cu5,
+    cu6,
+    f1,
+    f2,
+    f3,
+    f4,
+    f5,
+    f6,
+    f7,
+    f8,
+    f9,
+    f10,
+    fi1,
+    fi2,
+    fi3,
+    fi4,
+    fi5,
+    i1,
+    i2,
+    i3,
+    i4,
+    i5,
+    i6,
+    i7,
+    r1,
+    r2,
+    r3,
+    r4,
+    r5,
+    logo,
+    heroBg,
+    NotFound,
+    delivery,
+    chef1,
+    cheff,
+    avatar,
+    emptyCart,
+};
